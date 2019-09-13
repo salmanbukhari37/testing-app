e981871652142a4258ec4ce2fb5aab4b704b9432
Salman Bukhari <salmanbukhari37@gmail.com>
2019-09-13 18:42:05 +0500

    sa

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
-const app = express();
+
 const path = require("path");
+
+const app = express();
 
 // Run the app by serving the static files
 // in the dist directory
