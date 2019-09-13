b7af611a09664fab9e22c1726f54641e8fb03062
Salman Bukhari <salmanbukhari37@gmail.com>
2019-09-13 18:58:58 +0500

    asd

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const app = express();
 
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + "/src"));
+app.use(express.static(__dirname + "/dist/testing-app"));
 
 app.get("/*", function(req, res) {
-  res.sendFile(path.join(__dirname + "/src/index.html"));
+  res.sendFile(path.join(__dirname + "/dist/testing-app/index.html"));
 });
 
 // Start the app by listening on the default
